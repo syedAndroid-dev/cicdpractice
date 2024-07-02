b5ffbf266f68d87059042e98513ccf73f16ade95
syedAndroid-dev <syedabuthahir304@gmail.com>
2024-07-02 17:49:36 +0530

    initial commit

---
+++ app/src/main/java/com/example/cicdpractice/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.cicdpractice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.cicdpractice.ui.theme.CiCDPracticeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CiCDPracticeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                            name = "Android",
+                            modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    Column {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Spacer(
+            modifier = Modifier.padding(10.dp)
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CiCDPracticeTheme {
+        Greeting("Android")
+    }
+}
